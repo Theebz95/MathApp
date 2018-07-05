1a195f938a829bdf3990da9fef8566171ec98985
Piratheeban Annamalai <piratheeban95@hotmail.com>
2018-07-04 21:32:19 -0400

    addd vertication pundai

--- math-app/src/Container.js
+++ math-app/src/Container.js
@@ -7,7 +7,8 @@ class Container extends Component {
         second:0,
         level:0,
         time:30,
-        signArray:["+", "-", "x"]
+        signArray:["+", "-", "x"],
+        selectedSign:"",
     }
     
   generateSign(){
@@ -23,18 +24,62 @@ class Container extends Component {
     return numbers;
   }
 
-  componentDidMount(){
-      const randomNum = this.generateNumbers(10)
+  restart(){
+    const randomNum = this.generateNumbers(10);
+    const randomSign = this.generateSign();
       this.setState({
         first:randomNum.first,
         second:randomNum.second,
+        selectedSign: randomSign,
       })
+  }
+
+  componentDidMount(){
+    this.restart();
+    console.log(this.state.selectedSign);
+  }
+
+  validateAnswer(answer){
+    const parsedAnswer = parseInt(answer);
+    console.log(typeof answer);
+    console.log(this.state.selectedSign);
+    switch(this.state.selectedSign) {
+      case "+":
+          
+          if((this.state.first + this.state.second) === parsedAnswer)
+          {
+            console.log(this.state.first + this.state.second);
+            this.restart();
+          }
+          else{
+            alert("Wrong Answer!");
+          }
+          break;
+      case "-":
+          if (this.state.first - this.state.second === parsedAnswer) {
+            this.restart();
+          }
+          else{
+            alert("Wrong Answer!");
+          }
+          break;
+      case "x":
+          if (this.state.first * this.state.second === parsedAnswer) {
+            this.restart();
+          }
+          else{
+            alert("Wrong Answer!");
+          }
+          break;
+      default:
+          null;
+    }
   }
 
   render() {
     return (
       <div className="Math-Container">
-        <MathCards first={this.state.first} sign={this.generateSign()} second={this.state.second} />
+        <MathCards first={this.state.first} sign={this.state.selectedSign} second={this.state.second} validateAnswer={(answer)=>this.validateAnswer(answer)} />
       </div>
     );
   }
--- math-app/src/MathCards.js
+++ math-app/src/MathCards.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import { Card, CardTitle, CardText, Slider } from 'react-md';
+import { Card, CardTitle, CardText, Slider, Button, CardActions, TextField } from 'react-md';
 const style = { maxWidth: 320 };
 
 class MathCards extends Component {
 
     constructor(props){
         super(props);
+        this.state={
+            input:null,
+        };
     }
-
+    sendAnswers(){
+        this.props.validateAnswer(this.state.input);
+    }
     render() {
         return (
             <Card style={style} className="md-block-centered">
                 <CardTitle title="Using CardTitle" subtitle="With CardText" />
                 <CardText>
                     <h1>Kenny is Fat</h1>
-                    {this.props.first} {this.props.sign} {this.props.second}
+                    <h2>{this.props.first} {this.props.sign} {this.props.second}</h2>
+                    <br />
+                    <TextField
+                        id="answer"
+                        type="number"
+                        value={this.state.input}
+                        onChange={(value) => this.setState({input:value})}
+                        required
+                    />
                 </CardText>
+                <CardActions className="md-divider-border md-divider-border--top">
+                    <Button raised primary onClick={()=>this.sendAnswers()}>Submit Answer</Button>
+                </CardActions>
             </Card>
         );
     }
